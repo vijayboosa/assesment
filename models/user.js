6f9050c9d023c94407fcd5cce315732f7c96c2d5
vijayboosa <vijaysainagavamsi@gmail.com>
2025-03-20 16:03:53 +0530

    implement ulid as primary key

--- models/user.js
+++ models/user.js
@@ -1,8 +1,18 @@
 // models/user.js
+import { ulid } from 'ulid';
 import bcrypt from 'bcryptjs';
 
+// ULIDs are generated in a time-sortable manner which means that new entries are roughly in order
+// adding ulid's just to the users table just to show that we can have unique primary keys other than plain integer id's
+// there are downsides to ulid's when the application is scaled.
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING(26),
+      defaultValue: () => ulid(),
+      primaryKey: true,
+      allowNull: false,
+    },
     username: {
       type: DataTypes.STRING,
       unique: true,
